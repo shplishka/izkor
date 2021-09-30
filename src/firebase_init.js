ce251e0c0696f021157bced8d4656069f1fbb171
Mishka <shplishka@gmail.com>
2021-09-30 06:49:38 +0300

    first commit

--- src/firebase_init.js
+++ src/firebase_init.js
@@ -1,15 +1,14 @@
 import Firebase from "firebase";
 
 const FireaseConfig = Firebase.initializeApp({
-  // copy and paste the config code here of firebase
-  // apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-  // authDomain: "xxxxxxxxxxx.firebaseapp.com",
-  // databaseURL: "https://xxxxxxxxxxxxxxx.firebaseio.com",
-  // projectId: "xxxxxxxxxxxxxxxxxx",
-  // storageBucket: "xxxxxxxxxxxxx.appspot.com",
-  // messagingSenderId: "xxxxxxxxxxxxxxxxxxx",
-  // appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-  // measurementId: "xxxxxxxxxxxxx",
+  apiKey: "AIzaSyA74MlsGp-nxeuyAGMhnScGniOhI_5t8CE",
+  authDomain: "benpo-5f6ce.firebaseapp.com",
+  databaseURL: "https://benpo-5f6ce-default-rtdb.firebaseio.com/",
+  projectId: "benpo-5f6ce",
+  storageBucket: "benpo-5f6ce.appspot.com", 
+  messagingSenderId: "163691803576",
+  appId: "1:163691803576:web:aa54a6ebd7a7bc39e1554d",
+  measurementId: "G-R1H5NJ5SMX"
 });
 
 const database = FireaseConfig.firestore();
